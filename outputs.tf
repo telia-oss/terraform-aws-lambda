e1891051e8e95ec011640d90fe6daa0c8024fb3a
Mohamad al moualem <m.almouallem@gmail.com>
2018-10-31 09:40:27 +0100

    Use build in element function

--- outputs.tf
+++ outputs.tf
@@ -23,5 +23,5 @@ output "role_arn" {
 
 output "security_group_id" {
   description = "The ID of the security group."
-  value       = "${aws_security_group.vpc.*.id[0]}"
+  value       = "${element(aws_security_group.vpc.*.id, 0)}"
 }
