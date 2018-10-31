e0d7ebba3c910e7349808039ad93e9d67f4bccd6
Mohamad al moualem <m.almouallem@gmail.com>
2018-10-31 10:20:10 +0100

    Fix: output for lambda outside vpc

--- outputs.tf
+++ outputs.tf
@@ -23,5 +23,5 @@ output "role_arn" {
 
 output "security_group_id" {
   description = "The ID of the security group."
-  value       = "${element(aws_security_group.vpc.*.id, 0)}"
+  value       = "${element(concat(aws_security_group.vpc.*.id, list("")), 0)}"
 }
