80d65e494844c181c02b404e627d149d3a75d492
Rickard Löfström <rickard.lofstrom@gmail.com>
2019-05-03 19:38:24 +0200

    fix: add missing output for variances

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 output "arn" {
   description = "The Amazon Resource Name (ARN) identifying your Lambda Function."
-  value       = "${element(concat(aws_lambda_function.main.*.arn, aws_lambda_function.vpc.*.arn, aws_lambda_function.main_s3.*.arn, aws_lambda_function.vpc_s3.*.arn,), 0)}"
+  value       = "${element(concat(aws_lambda_function.main.*.arn, aws_lambda_function.vpc.*.arn, aws_lambda_function.main_s3.*.arn, aws_lambda_function.main_s3_source_code_hash.*.arn, aws_lambda_function.vpc_s3.*.arn, aws_lambda_function.vpc_s3_source_code_hash.*.arn,), 0)}"
 }
 
 output "name" {
@@ -28,10 +28,10 @@ output "security_group_id" {
 
 output "invoke_arn" {
   description = " The ARN to be used for invoking Lambda Function from API Gateway - to be used in aws_api_gateway_integration uri."
-  value       = "${element(concat(aws_lambda_function.main.*.invoke_arn, aws_lambda_function.vpc.*.invoke_arn, aws_lambda_function.main_s3.*.invoke_arn, aws_lambda_function.vpc_s3.*.invoke_arn,), 0)}"
+  value       = "${element(concat(aws_lambda_function.main.*.invoke_arn, aws_lambda_function.vpc.*.invoke_arn, aws_lambda_function.main_s3.*.invoke_arn, aws_lambda_function.main_s3_source_code_hash.*.invoke_arn, aws_lambda_function.vpc_s3.*.invoke_arn, aws_lambda_function.vpc_s3_source_code_hash.*.invoke_arn,), 0)}"
 }
 
 output "qualified_arn" {
   description = " The Amazon Resource Name (ARN) identifying your Lambda Function Version (if versioning is enabled via publish = true)."
-  value       = "${element(concat(aws_lambda_function.main.*.qualified_arn, aws_lambda_function.vpc.*.qualified_arn, aws_lambda_function.main_s3.*.qualified_arn, aws_lambda_function.vpc_s3.*.qualified_arn,), 0)}"
+  value       = "${element(concat(aws_lambda_function.main.*.qualified_arn, aws_lambda_function.vpc.*.qualified_arn, aws_lambda_function.main_s3.*.qualified_arn, aws_lambda_function.main_s3_source_code_hash.*.qualified_arn, aws_lambda_function.vpc_s3.*.qualified_arn, aws_lambda_function.vpc_s3_source_code_hash.*.qualified_arn,), 0)}"
 }
