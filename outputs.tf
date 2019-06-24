0fadcd28777fc042ce93dbfde8614619e80c23e5
Stijn De Haes <stijndehaes@gmail.com>
2019-06-24 08:17:58 +0200

    Upgrade to terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -3,35 +3,35 @@
 # ------------------------------------------------------------------------------
 output "arn" {
   description = "The Amazon Resource Name (ARN) identifying your Lambda Function."
-  value       = "${element(concat(aws_lambda_function.main.*.arn, aws_lambda_function.vpc.*.arn, aws_lambda_function.main_s3.*.arn, aws_lambda_function.vpc_s3.*.arn,), 0)}"
+  value       = concat(aws_lambda_function.main.*.arn, aws_lambda_function.vpc.*.arn, aws_lambda_function.main_s3.*.arn, aws_lambda_function.vpc_s3.*.arn)[0]
 }
 
 output "name" {
   description = "The name of the lambda function."
-  value       = "${var.name_prefix}"
+  value       = var.name_prefix
 }
 
 output "role_name" {
   description = "The name of the execution role."
-  value       = "${aws_iam_role.main.name}"
+  value       = aws_iam_role.main.name
 }
 
 output "role_arn" {
   description = "The Amazon Resource Name (ARN) specifying the execution role."
-  value       = "${aws_iam_role.main.arn}"
+  value       = aws_iam_role.main.arn
 }
 
 output "security_group_id" {
   description = "The ID of the security group."
-  value       = "${element(concat(aws_security_group.vpc.*.id, list("")), 0)}"
+  value       = concat(aws_security_group.vpc.*.id, list(""))[0]
 }
 
 output "invoke_arn" {
   description = " The ARN to be used for invoking Lambda Function from API Gateway - to be used in aws_api_gateway_integration uri."
-  value       = "${element(concat(aws_lambda_function.main.*.invoke_arn, aws_lambda_function.vpc.*.invoke_arn, aws_lambda_function.main_s3.*.invoke_arn, aws_lambda_function.vpc_s3.*.invoke_arn,), 0)}"
+  value       = concat(aws_lambda_function.main.*.invoke_arn, aws_lambda_function.vpc.*.invoke_arn, aws_lambda_function.main_s3.*.invoke_arn, aws_lambda_function.vpc_s3.*.invoke_arn)[0]
 }
 
 output "qualified_arn" {
   description = " The Amazon Resource Name (ARN) identifying your Lambda Function Version (if versioning is enabled via publish = true)."
-  value       = "${element(concat(aws_lambda_function.main.*.qualified_arn, aws_lambda_function.vpc.*.qualified_arn, aws_lambda_function.main_s3.*.qualified_arn, aws_lambda_function.vpc_s3.*.qualified_arn,), 0)}"
+  value       = concat(aws_lambda_function.main.*.qualified_arn, aws_lambda_function.vpc.*.qualified_arn, aws_lambda_function.main_s3.*.qualified_arn, aws_lambda_function.vpc_s3.*.qualified_arn)[0]
 }
