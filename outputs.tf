bef13ec7d8b3fcefc414433003eb3b921ac0a02d
itsdalmo <kristian@doingit.no>
2019-07-30 16:17:42 +0200

    Reoganise output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,16 @@ output "arn" {
   value       = aws_lambda_function.main.arn
 }
 
+output "invoke_arn" {
+  description = " The ARN to be used for invoking Lambda Function from API Gateway - to be used in aws_api_gateway_integration uri."
+  value       = aws_lambda_function.main.invoke_arn
+}
+
+output "qualified_arn" {
+  description = " The Amazon Resource Name (ARN) identifying your Lambda Function Version (if versioning is enabled via publish = true)."
+  value       = aws_lambda_function.main.qualified_arn
+}
+
 output "name" {
   description = "The name of the lambda function."
   value       = var.name_prefix
@@ -24,15 +34,5 @@ output "role_arn" {
 output "security_group_id" {
   description = "The ID of the security group."
   value       = concat(aws_security_group.vpc[*].id, [""])[0]
-}
-
-output "invoke_arn" {
-  description = " The ARN to be used for invoking Lambda Function from API Gateway - to be used in aws_api_gateway_integration uri."
-  value       = aws_lambda_function.main.invoke_arn
-}
-
-output "qualified_arn" {
-  description = " The Amazon Resource Name (ARN) identifying your Lambda Function Version (if versioning is enabled via publish = true)."
-  value       = aws_lambda_function.main.qualified_arn
 }
 
