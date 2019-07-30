5f746a9829d70e609e7954b21ba47ea9102a26d5
itsdalmo <kristian@doingit.no>
2019-07-30 14:23:36 +0200

    Use new splat syntax in output

--- outputs.tf
+++ outputs.tf
@@ -3,16 +3,13 @@
 # ------------------------------------------------------------------------------
 output "arn" {
   description = "The Amazon Resource Name (ARN) identifying your Lambda Function."
-  value = element(
-    concat(
-      aws_lambda_function.main.*.arn,
-      aws_lambda_function.vpc.*.arn,
-      aws_lambda_function.main_s3.*.arn,
-      aws_lambda_function.vpc_s3.*.arn,
-      [""],
-    ),
-    0,
-  )
+  value = concat(
+    aws_lambda_function.main[*].arn,
+    aws_lambda_function.vpc[*].arn,
+    aws_lambda_function.main_s3[*].arn,
+    aws_lambda_function.vpc_s3[*].arn,
+    [""],
+  )[0]
 }
 
 output "name" {
@@ -32,34 +29,28 @@ output "role_arn" {
 
 output "security_group_id" {
   description = "The ID of the security group."
-  value       = element(concat(aws_security_group.vpc.*.id, [""]), 0)
+  value       = concat(aws_security_group.vpc[*].id, [""])[0]
 }
 
 output "invoke_arn" {
   description = " The ARN to be used for invoking Lambda Function from API Gateway - to be used in aws_api_gateway_integration uri."
-  value = element(
-    concat(
-      aws_lambda_function.main.*.invoke_arn,
-      aws_lambda_function.vpc.*.invoke_arn,
-      aws_lambda_function.main_s3.*.invoke_arn,
-      aws_lambda_function.vpc_s3.*.invoke_arn,
-      [""],
-    ),
-    0,
-  )
+  value = concat(
+    aws_lambda_function.main[*].invoke_arn,
+    aws_lambda_function.vpc[*].invoke_arn,
+    aws_lambda_function.main_s3[*].invoke_arn,
+    aws_lambda_function.vpc_s3[*].invoke_arn,
+    [""],
+  )[0]
 }
 
 output "qualified_arn" {
   description = " The Amazon Resource Name (ARN) identifying your Lambda Function Version (if versioning is enabled via publish = true)."
-  value = element(
-    concat(
-      aws_lambda_function.main.*.qualified_arn,
-      aws_lambda_function.vpc.*.qualified_arn,
-      aws_lambda_function.main_s3.*.qualified_arn,
-      aws_lambda_function.vpc_s3.*.qualified_arn,
-      [""],
-    ),
-    0,
-  )
+  value = concat(
+    aws_lambda_function.main[*].qualified_arn,
+    aws_lambda_function.vpc[*].qualified_arn,
+    aws_lambda_function.main_s3[*].qualified_arn,
+    aws_lambda_function.vpc_s3[*].qualified_arn,
+    [""],
+  )[0]
 }
 
